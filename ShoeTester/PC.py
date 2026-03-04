c8f1db3714a5a987f6d9b66cd6ed28b761256e0c
Wachirawit Tanleng <134188500+X-zeed@users.noreply.github.com>
2026-03-04 12:32:07 +0700

    Update PC.py

--- ShoeTester/PC.py
+++ ShoeTester/PC.py
@@ -1,23 +1,27 @@
 import serial
 import json
 
-# เปลี่ยน COM ให้ตรงกับของคุณ
-ser = serial.Serial("COM7", 9600, timeout=1)
+PORT = "COM8"
 
-print("Connected to ESP32...")
+ser = serial.Serial(PORT, 9600, timeout=1)
+
+print("Connected to ESP32-C3...")
 
 while True:
     if ser.in_waiting > 0:
         data = ser.readline().decode("utf-8").strip()
-        
+
         try:
             # แปลง string เป็น JSON
             json_data = json.loads(data)
-            
-            # ดึงค่า shoe
+
+            # ดึงค่า
             shoe_value = json_data["shoe"]
-            
+            status_value = json_data["status"]
+
             print("Shoe Value:", shoe_value)
+            print("Status:", status_value)
+            print("------------------")
 
         except:
             # ถ้าไม่ใช่ JSON จะข้ามไป
